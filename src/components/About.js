a2023f8594556b6e06cfda5a4c934e4fcf40c9e4
Brett Polacek <brettpolacek@gmail.com>
2023-02-21 06:45:21 -0500

    updating js

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">About
+    <h2>About Me</h2>
+    <p>good morning! Welcome to my page.</p>
+    <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this"></img>
+    </div>
 }
 
 export default About;
